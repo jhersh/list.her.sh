6a96b5fc613cefe798e0a1b5253a94c26f49beef
Jonathan Hersh <jon@her.sh>
2024-12-01 14:16:58 -0800

    Add post tags!

---
+++ _plugins/tag-generator.rb
@@ -0,0 +1,16 @@
+Jekyll::Hooks.register :posts, :post_write do |post|
+  all_existing_tags = Dir.entries("_tags")
+    .map { |t| t.match(/(.*).md/) }
+    .compact.map { |m| m[1] }
+
+  tags = post['tags'].reject { |t| t.empty? }
+  tags.each do |tag|
+    generate_tag_file(tag) if !all_existing_tags.include?(tag)
+  end
+end
+
+def generate_tag_file(tag)
+  File.open("_tags/#{tag}.md", "wb") do |file|
+    file << "---\nlayout: tags\ntag-name: #{tag}\n---\n"
+  end
+end
